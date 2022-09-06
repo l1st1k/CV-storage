9e09cecb31502d0442fa4621f280a2825f9f537d
l1st1k <listik.mp@gmail.com>
2022-09-06 17:53:52 +0300

    added examples

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ import boto3
 
 from dotenv import dotenv_values
 
+
 __all__ = ('db_table',)
 
 
@@ -11,7 +12,6 @@ DYNAMODB_ENDPOINT = config["DYNAMODB_ENDPOINT"]
 AWS_ACCESS_KEY = config["AWS_ACCESS_KEY"]
 AWS_SECRET_ACCESS_KEY = config["AWS_SECRET_ACCESS_KEY"]
 AWS_REGION = config["AWS_REGION"]
-
 
 # Client
 dynamo_client = boto3.client("dynamodb", endpoint_url=DYNAMODB_ENDPOINT, aws_access_key_id=AWS_ACCESS_KEY,
