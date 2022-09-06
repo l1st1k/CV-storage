ed661cd0f6c695b37055e2f98f8df6a5e58a3a30
l1st1k <listik.mp@gmail.com>
2022-09-06 15:40:38 +0300

    connected boto3 to dynamoDB. Installed terraform with all dependencies

---
+++ database.py
@@ -0,0 +1,21 @@
+import boto3
+
+from dotenv import dotenv_values
+
+__all__ = ('db_table',)
+
+
+# Env variables
+config = dotenv_values(".env")
+DYNAMODB_ENDPOINT = config["DYNAMODB_ENDPOINT"]
+AWS_ACCESS_KEY = config["AWS_ACCESS_KEY"]
+AWS_SECRET_ACCESS_KEY = config["AWS_SECRET_ACCESS_KEY"]
+AWS_REGION = config["AWS_REGION"]
+
+
+# Client
+dynamo_client = boto3.client("dynamodb", endpoint_url=DYNAMODB_ENDPOINT, aws_access_key_id=AWS_ACCESS_KEY,
+                             aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION)
+
+# Table
+db_table = dynamo_client.Table('main_table')
---
+++ main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+
+  access_key                  = "foo"
+  secret_key                  = "foo"
+  region                      = "eu-central-1"
+
+  s3_force_path_style         = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    dynamodb             = "http://localhost:4566"
+  }
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "GameScores"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "CV_id"
+
+  attribute {
+    name = "CV_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+  attribute {
+    name = "TopScore"
+    type = "N"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+  global_secondary_index {
+    name               = "GameTitleIndex"
+    hash_key           = "GameTitle"
+    range_key          = "TopScore"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
+
+  tags = {
+    Name        = "dynamodb-main-table"
+    Environment = "production"
+  }
+}
