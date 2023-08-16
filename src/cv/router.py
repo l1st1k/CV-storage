4b560d558da313c68859e78084033baab82e2d61
l1st1k <listik.mp@gmail.com>
2023-08-16 13:11:56 +0300

    restructured into routers

---
+++ src/cv/router.py
@@ -0,0 +1,46 @@
+from fastapi import UploadFile, Query
+from fastapi.responses import JSONResponse, FileResponse
+
+from cv.repository import CVRepository
+from cv.models import *
+
+
+class CVRouter:
+    tags = ("CV",)
+    
+    def __init__(self, app):
+        self.app = app
+
+    def configure_routes(self):
+        self.app.get("/cvs", response_model=CVsRead, tags=self.tags)(self.list_cvs)
+        self.app.get("/cv/{cv_id}", response_model=CVFullRead, tags=self.tags)(self.get_cv)
+        self.app.post("/cv", response_class=JSONResponse, tags=self.tags)(self.post_cv)
+        self.app.patch("/cv/{cv_id}", response_model=CVFullRead, tags=self.tags)(self.update_cv)
+        self.app.get("/cv/{cv_id}/csv", response_class=FileResponse, tags=self.tags)(self.get_csv)
+        self.app.delete("/cv/{cv_id}", response_class=JSONResponse, tags=self.tags)(self.delete_cv)
+        self.app.get("/cvs/search", response_model=CVsFullRead, tags=self.tags)(self.search_cvs)
+
+    async def list_cvs(self):
+        return CVRepository.list()
+
+    async def get_cv(self, cv_id: str):
+        return CVRepository.get(cv_id=cv_id)
+
+    async def post_cv(self, file: UploadFile):
+        return CVRepository.create(file=file)
+
+    async def update_cv(self, cv_id: str, model: CVUpdate):
+        return CVRepository.update(cv_id=cv_id, data=model)
+
+    async def get_csv(self, cv_id: str):
+        return CVRepository.get_csv(cv_id=cv_id)
+
+    async def delete_cv(self, cv_id: str):
+        return CVRepository.delete(cv_id=cv_id)
+
+    async def search_cvs(self, skill: str = Query(default=''), last_name: str = Query(default='', max_length=25), 
+                         major: str = Query(default='', max_length=25)):
+        skill = skill.lower()
+        last_name = last_name.lower()
+        major = major.lower()
+        return CVRepository.search(skill=skill, last_name=last_name, major=major)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI, Query, UploadFile
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse, JSONResponse, PlainTextResponse
+from fastapi.responses import PlainTextResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
-from cv.models import CVFullRead, CVsFullRead, CVsRead, CVUpdate
-from cv.repository import CVRepository
+from cv.router import CVRouter
 
 app = FastAPI()
 
@@ -26,93 +25,10 @@ app.add_middleware(
 async def http_exception_handler(request, exc):
     return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
 
+routers = [
+    CVRouter,
+]
 
-@app.get(
-    "/cvs",
-    response_model=CVsRead,
-    description="List all the CVs",
-    tags=[
-        "CV"
-    ]
-)
-def _list_cvs():
-    return CVRepository.list()
-
-
-@app.get(
-    "/cv/{cv_id}",
-    response_model=CVFullRead,
-    description="Retrieve CV by ID",
-    tags=[
-        "CV"
-    ]
-)
-def _get_cv(cv_id: str):
-    return CVRepository.get(cv_id=cv_id)
-
-
-@app.post(
-    "/cv",
-    response_class=JSONResponse,
-    description='Upload CV in .csv',
-    tags=[
-        "CV"
-    ]
-)
-def _post_cv(file: UploadFile):
-    return CVRepository.create(file=file)
-
-
-@app.patch(
-    "/cv/{cv_id}",
-    response_model=CVFullRead,
-    description="Updates CV by ID",
-    tags=[
-        "CV"
-    ]
-)
-def _update_cv(cv_id: str, model: CVUpdate):
-    return CVRepository.update(cv_id=cv_id, data=model)
-
-
-@app.get(
-    "/cv/{cv_id}/csv",
-    response_class=FileResponse,
-    description='Returns CV in .csv file',
-    tags=[
-        "CV"
-    ]
-)
-def _get_csv(cv_id: str):
-    return CVRepository.get_csv(cv_id=cv_id)
-
-
-@app.delete(
-    "/cv/{cv_id}",
-    response_class=JSONResponse,
-    description='Deletes CV from database',
-    tags=[
-        "CV"
-    ]
-)
-def _delete_csv(cv_id: str):
-    return CVRepository.delete(cv_id=cv_id)
-
-
-@app.get(
-    "/cvs/search",
-    response_model=CVsFullRead,
-    description='Searches for CV with necessary skills',
-    tags=[
-        "CV"
-    ]
-)
-def _search_cvs(
-        skill: str = Query(default=''),
-        last_name: str = Query(default='', max_length=25),
-        major: str = Query(default='', max_length=25)
-):
-    skill = skill.lower()
-    last_name = last_name.lower()
-    major = major.lower()
-    return CVRepository.search(skill=skill, last_name=last_name, major=major)
+for router in routers:
+    new_router = router(app)
+    new_router.configure_routes()
