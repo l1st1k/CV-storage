4cb907a5cf4f43a280690e87a7823ddc7c669f70
l1st1k <listik.mp@gmail.com>
2023-09-12 11:50:47 +0300

    type hints for dynamoDB tables

--- src/core/database.py
+++ src/core/database.py
@@ -1,7 +1,11 @@
 import boto3
 
-from core.config import (AWS_ACCESS_KEY, AWS_REGION, AWS_SECRET_ACCESS_KEY,
-                    DYNAMODB_ENDPOINT)
+from mypy_boto3_dynamodb.service_resource import Table, DynamoDBServiceResource
+
+from core.config import (AWS_ACCESS_KEY,
+                         AWS_REGION,
+                         AWS_SECRET_ACCESS_KEY,
+                         DYNAMODB_ENDPOINT)
 
 __all__ = (
     'cv_table',
@@ -12,11 +16,14 @@ __all__ = (
 
 
 # Resource
-dynamo_resource = boto3.resource("dynamodb", endpoint_url=DYNAMODB_ENDPOINT, aws_access_key_id=AWS_ACCESS_KEY,
-                                 aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION)
+dynamo_resource: DynamoDBServiceResource = boto3.resource("dynamodb",
+                                                          endpoint_url=DYNAMODB_ENDPOINT,
+                                                          aws_access_key_id=AWS_ACCESS_KEY,
+                                                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                                                          region_name=AWS_REGION)
 
 # Tables
-cv_table = dynamo_resource.Table('cv_table')
-company_table = dynamo_resource.Table('company_table')
-manager_table = dynamo_resource.Table('manager_table')
-vacancy_table = dynamo_resource.Table('vacancy_table')
+cv_table: Table = dynamo_resource.Table('cv_table')
+company_table: Table = dynamo_resource.Table('company_table')
+manager_table: Table = dynamo_resource.Table('manager_table')
+vacancy_table: Table = dynamo_resource.Table('vacancy_table')
