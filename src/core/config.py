a7ba6dcf41de1805c46e6f84760c24754fa3dec3
Aliaksandr Listvanovich <aliaksandr.listvanovich@superbo.ai>
2024-02-28 12:24:13 +0200

    env vars

--- src/core/config.py
+++ src/core/config.py
@@ -1,10 +1,6 @@
 from dotenv import dotenv_values
 
 __all__ = (
-    'DYNAMODB_ENDPOINT',
-    'AWS_ACCESS_KEY',
-    'AWS_SECRET_ACCESS_KEY',
-    'AWS_REGION',
     'AUTH_KEY',
     'POSTGRES_USER',
     'POSTGRES_PASSWORD',
@@ -15,10 +11,7 @@ __all__ = (
 config = dotenv_values("src/.env")  # TODO remove local path
 # config = dotenv_values(".env")
 
-DYNAMODB_ENDPOINT = config["DYNAMODB_ENDPOINT"]
-AWS_ACCESS_KEY = config["AWS_ACCESS_KEY"]
-AWS_SECRET_ACCESS_KEY = config["AWS_SECRET_ACCESS_KEY"]
-AWS_REGION = config["AWS_REGION"]
+
 AUTH_KEY = config["AUTH_KEY"]
 POSTGRES_USER = config["POSTGRES_USER"]
 POSTGRES_PASSWORD = config["POSTGRES_PASSWORD"]
