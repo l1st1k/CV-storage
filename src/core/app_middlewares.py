e51a77e5882e027839a21613e3148e7a15b9cd54
Aliaksandr Listvanovich <aliaksandr.listvanovich@superbo.ai>
2024-04-23 16:28:11 +0300

    mock for auth cookies

--- src/core/app_middlewares.py
+++ src/core/app_middlewares.py
@@ -1,12 +1,25 @@
 import logging
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from starlette.datastructures import MutableHeaders
 
 __all__ = ('configure_app_middlewares',)
 
 logger = logging.getLogger(__name__)
+
+
+class CustomRequest(Request):
+    # TODO remove this bullshit =)))) (just a mock)
+
+    def set_cookies_from_header(self):
+        cookie_value = self.headers.get('cooookie')
+        if cookie_value:
+            # Assuming the value is a simple string; you may need to parse if it's formatted differently
+            mutable_headers = MutableHeaders(raw=self.scope['headers'])
+            for cookie in cookie_value.split(" "):
+                mutable_headers.append('cookie', cookie)
 
 
 def configure_app_middlewares(application: FastAPI) -> None:
@@ -25,8 +38,11 @@ def configure_app_middlewares(application: FastAPI) -> None:
 
     @application.middleware("http")
     async def cookie_logger(request, call_next):
-        logger.info(f"Request Cookies: {request.cookies}")
-        response = await call_next(request)
+        custom_request = CustomRequest(request.scope)
+        custom_request.set_cookies_from_header()
+        logger.info(f"Request Cookies: {custom_request.cookies}")
+
+        response = await call_next(custom_request)
         response_cookies = response.headers.getlist('set-cookie')
         logger.info(f"Response Cookies: {response_cookies}")
         if response_cookies:
